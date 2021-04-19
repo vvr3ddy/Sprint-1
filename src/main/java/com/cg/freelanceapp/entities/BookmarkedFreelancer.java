c570514822c08f10f09178811b1afccc67d3adab
vvr3ddy <vvr-rockstar@bmsit.in>
2021-04-19 09:22:50 +0530

    :octocat: Update BookmarkedFreelancer.java
    Create entity relationships
    
    Signed-off-by: vvr3ddy <vvr-rockstar@bmsit.in>

--- src/main/java/com/cg/freelanceapp/entities/BookmarkedFreelancer.java
+++ src/main/java/com/cg/freelanceapp/entities/BookmarkedFreelancer.java
@@ -1,8 +1,61 @@
 package com.cg.freelanceapp.entities;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class BookmarkedFreelancer {
-	Long id;
-	Skill skill;
-	Freelancer freelancer;
-	Recruiter bookmarkedBy;
+	private Long id;
+	private Skill skill;
+	private Freelancer freelancer;
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "recruiter_id")
+	private Recruiter bookmarkedBy;
+
+	public BookmarkedFreelancer() {
+		super();
+	}
+
+	public BookmarkedFreelancer(Long id, Skill skill, Freelancer freelancer, Recruiter bookmarkedBy) {
+		super();
+		this.id = id;
+		this.skill = skill;
+		this.freelancer = freelancer;
+		this.bookmarkedBy = bookmarkedBy;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Skill getSkill() {
+		return skill;
+	}
+
+	public void setSkill(Skill skill) {
+		this.skill = skill;
+	}
+
+	public Freelancer getFreelancer() {
+		return freelancer;
+	}
+
+	public void setFreelancer(Freelancer freelancer) {
+		this.freelancer = freelancer;
+	}
+
+	public Recruiter getBookmarkedBy() {
+		return bookmarkedBy;
+	}
+
+	public void setBookmarkedBy(Recruiter bookmarkedBy) {
+		this.bookmarkedBy = bookmarkedBy;
+	}
+
 }
